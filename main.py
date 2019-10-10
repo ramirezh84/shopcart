3cca22cb2a2aba4544275c41c6264a5756d880bd
Enrique Ramirez <hurami@amazon.com>
2019-10-09 22:11:07 -0700

    Fix 1

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'devstack3-dbcluster-xp4up3zn8jhh.cluster-cz
 mysql = MySQL(app)
 
 def getLoginDetails():
-    with mysql.connect('database.db') as conn:
+    with mysql.connect() as conn:
         cur = conn.cursor()
         if 'email' not in session:
             loggedIn = False
@@ -35,7 +35,7 @@ def getLoginDetails():
 @app.route("/")
 def root():
     loggedIn, firstName, noOfItems = getLoginDetails()
-    with mysql.connect('database.db') as conn:
+    with mysql.connect() as conn:
         cur = conn.cursor()
         cur.execute('SELECT productId, name, price, description, image, stock FROM products')
         itemData = cur.fetchall()
@@ -46,7 +46,7 @@ def root():
 
 @app.route("/add")
 def admin():
-    with mysql.connect('database.db') as conn:
+    with mysql.connect() as conn:
         cur = conn.cursor()
         cur.execute("SELECT categoryId, name FROM categories")
         categories = cur.fetchall()
@@ -68,7 +68,7 @@ def addItem():
             filename = secure_filename(image.filename)
             image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         imagename = filename
-        with mysql.connect('database.db') as conn:
+        with mysql.connect() as conn:
             try:
                 cur = conn.cursor()
                 cur.execute('''INSERT INTO products (name, price, description, image, stock, categoryId) VALUES (?, ?, ?, ?, ?, ?)''', (name, price, description, imagename, stock, categoryId))
@@ -83,7 +83,7 @@ def addItem():
 
 @app.route("/remove")
 def remove():
-    with mysql.connect('database.db') as conn:
+    with mysql.connect() as conn:
         cur = conn.cursor()
         cur.execute('SELECT productId, name, price, description, image, stock FROM products')
         data = cur.fetchall()
@@ -93,7 +93,7 @@ def remove():
 @app.route("/removeItem")
 def removeItem():
     productId = request.args.get('productId')
-    with mysql.connect('database.db') as conn:
+    with mysql.connect() as conn:
         try:
             cur = conn.cursor()
             cur.execute('DELETE FROM products WHERE productID = ?', (productId, ))
@@ -110,7 +110,7 @@ def removeItem():
 def displayCategory():
         loggedIn, firstName, noOfItems = getLoginDetails()
         categoryId = request.args.get("categoryId")
-        with mysql.connect('database.db') as conn:
+        with mysql.connect() as conn:
             cur = conn.cursor()
             cur.execute("SELECT products.productId, products.name, products.price, products.image, categories.name FROM products, categories WHERE products.categoryId = categories.categoryId AND categories.categoryId = ?", (categoryId, ))
             data = cur.fetchall()
@@ -131,7 +131,7 @@ def editProfile():
     if 'email' not in session:
         return redirect(url_for('root'))
     loggedIn, firstName, noOfItems = getLoginDetails()
-    with mysql.connect('database.db') as conn:
+    with mysql.connect() as conn:
         cur = conn.cursor()
         cur.execute("SELECT userId, email, firstName, lastName, address1, address2, zipcode, city, state, country, phone FROM users WHERE email = ?", (session['email'], ))
         profileData = cur.fetchone()
@@ -147,7 +147,7 @@ def changePassword():
         oldPassword = hashlib.md5(oldPassword.encode()).hexdigest()
         newPassword = request.form['newpassword']
         newPassword = hashlib.md5(newPassword.encode()).hexdigest()
-        with mysql.connect('database.db') as conn:
+        with mysql.connect() as conn:
             cur = conn.cursor()
             cur.execute("SELECT userId, password FROM users WHERE email = ?", (session['email'], ))
             userId, password = cur.fetchone()
@@ -180,7 +180,7 @@ def updateProfile():
         state = request.form['state']
         country = request.form['country']
         phone = request.form['phone']
-        with mysql.connect('database.db') as con:
+        with mysql.connect() as con:
                 try:
                     cur = con.cursor()
                     cur.execute('UPDATE users SET firstName = ?, lastName = ?, address1 = ?, address2 = ?, zipcode = ?, city = ?, state = ?, country = ?, phone = ? WHERE email = ?', (firstName, lastName, address1, address2, zipcode, city, state, country, phone, email))
@@ -216,7 +216,7 @@ def login():
 def productDescription():
     loggedIn, firstName, noOfItems = getLoginDetails()
     productId = request.args.get('productId')
-    with mysql.connect('database.db') as conn:
+    with mysql.connect() as conn:
         cur = conn.cursor()
         cur.execute('SELECT productId, name, price, description, image, stock FROM products WHERE productId = ?', (productId, ))
         productData = cur.fetchone()
@@ -229,7 +229,7 @@ def addToCart():
         return redirect(url_for('loginForm'))
     else:
         productId = int(request.args.get('productId'))
-        with mysql.connect('database.db') as conn:
+        with mysql.connect() as conn:
             cur = conn.cursor()
             cur.execute("SELECT userId FROM users WHERE email = ?", (session['email'], ))
             userId = cur.fetchone()[0]
@@ -249,7 +249,7 @@ def cart():
         return redirect(url_for('loginForm'))
     loggedIn, firstName, noOfItems = getLoginDetails()
     email = session['email']
-    with mysql.connect('database.db') as conn:
+    with mysql.connect() as conn:
         cur = conn.cursor()
         cur.execute("SELECT userId FROM users WHERE email = ?", (email, ))
         userId = cur.fetchone()[0]
@@ -266,7 +266,7 @@ def removeFromCart():
         return redirect(url_for('loginForm'))
     email = session['email']
     productId = int(request.args.get('productId'))
-    with mysql.connect('database.db') as conn:
+    with mysql.connect() as conn:
         cur = conn.cursor()
         cur.execute("SELECT userId FROM users WHERE email = ?", (email, ))
         userId = cur.fetchone()[0]
@@ -286,7 +286,7 @@ def logout():
     return redirect(url_for('root'))
 
 def is_valid(email, password):
-    con = mysql.connect('database.db')
+    con = mysql.connect()
     cur = con.cursor()
     cur.execute('SELECT email, password FROM users')
     data = cur.fetchall()
@@ -311,7 +311,7 @@ def register():
         country = request.form['country']
         phone = request.form['phone']
 
-        with mysql.connect('database.db') as con:
+        with mysql.connect() as con:
             try:
                 cur = con.cursor()
                 cur.execute('INSERT INTO users (password, email, firstName, lastName, address1, address2, zipcode, city, state, country, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (hashlib.md5(password.encode()).hexdigest(), email, firstName, lastName, address1, address2, zipcode, city, state, country, phone))
